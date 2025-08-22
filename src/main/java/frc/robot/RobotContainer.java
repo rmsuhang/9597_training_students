c1947e68f7615ad8486ff3b071cefa60dc11cd43
Lufas-0311 <Lufas0311@hotmail.com>
2025-08-22 11:36:43 +0800

    final_test_one

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,7 +27,6 @@ import frc.robot.subsystems.drive;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final drive m_drive_subsystenm = new drive();
-  private final CANdleSystem m_candle_subsystenm = new CANdleSystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
